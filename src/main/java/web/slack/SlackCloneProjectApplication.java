e114fdf83b09737f13b6a604dfd714720f1e33da
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-09-24 18:52:32 +0900

    Chore: connect to mongodb

--- src/main/java/web/slack/SlackCloneProjectApplication.java
+++ src/main/java/web/slack/SlackCloneProjectApplication.java
@@ -2,7 +2,9 @@ package web.slack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @SpringBootApplication
 public class SlackCloneProjectApplication {
 
