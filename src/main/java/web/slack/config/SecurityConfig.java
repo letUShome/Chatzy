39df98859eba5ef01d039e99e8953f0221521fbd
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-08 01:17:50 +0900

    feat: CorsFilter 추가

--- src/main/java/web/slack/config/SecurityConfig.java
+++ src/main/java/web/slack/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package web.slack.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.filter.CorsFilter;
 import web.slack.config.handlers.CustomLoginSuccessHandler;
 import web.slack.config.handlers.CustomLogoutSuccessHandler;
 import web.slack.config.jwt.JwtAuthenticationFilter;
@@ -24,6 +27,7 @@ public class SecurityConfig{
     private final CustomLogoutSuccessHandler logoutSuccessHandler;
     private final CustomLoginSuccessHandler loginSuccessHandler;
     private final CustomOauth2UserService customOauth2UserService;
+    private final CorsFilter corsFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -38,7 +42,7 @@ public class SecurityConfig{
                         "/js/**", "/h2-console/**").permitAll() // 우선 전체허용
                 .antMatchers("/members").permitAll()
                 .antMatchers("/**").permitAll()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .logout()
                 .logoutSuccessHandler(logoutSuccessHandler)
@@ -49,6 +53,14 @@ public class SecurityConfig{
 
 
         return httpSecurity.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    public class CustomDsl extends AbstractHttpConfigurer<CustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder){
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder.addFilter(corsFilter);
+        }
     }
 
 }
--- src/main/java/web/slack/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/web/slack/config/jwt/JwtAuthenticationFilter.java
@@ -1,10 +1,10 @@
 package web.slack.config.jwt;
 
-import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+import web.slack.domain.repository.MemberRepository;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,7 +12,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.jar.JarException;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
@@ -29,6 +28,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         chain.doFilter(request, response);
     }
 }
