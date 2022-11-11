4a1e40948ef8f31b3ce0e98aa0311caf460eb66a
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-11 22:05:55 +0900

    !HOTFIX: PID 작동 안되는 문제 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,12 +13,12 @@ CURRENT_PID=$(pgrep -fla java | grep slack | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
+if [ -n "$CURRENT_PID" ]; then
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
+else
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 fi
 
 echo "> 새 어플리케이션 배포"
