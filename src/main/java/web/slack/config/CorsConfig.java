71c86be1f0768e0aaef0f9620b1ba1d602cf6966
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-11 15:58:20 +0900

    !HOTFIX: 로그인 url, CORS url 수정

--- src/main/java/web/slack/config/CorsConfig.java
+++ src/main/java/web/slack/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:3090");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setMaxAge(3600L);
