d01744ad9503be6c7f5543ec418a1571371bcc15
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-14 13:46:02 +0900

    fix: 이름 겹치는 Entity 네이밍

--- src/main/java/web/slack/domain/entity/BodyMessage.java
+++ src/main/java/web/slack/domain/entity/BodyMessage.java
@@ -1,17 +1,16 @@
 package web.slack.domain.entity;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
 
 @Data
 @Setter
-public class Message {
+public class BodyMessage {
     private StatusEnum status;
     private String message;
     private Object data;
 
-    public Message() {
+    public BodyMessage() {
         this.status = StatusEnum.BAD_REQUEST;
         this.data = null;
         this.message = null;
