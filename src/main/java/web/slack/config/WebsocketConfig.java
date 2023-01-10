a36c5eeb1c06b5d8f0ccef5ed8d2a79b9c3dcb4c
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2023-01-10 18:29:06 +0900

    Refactor: 사용하지 않는 라이브러리 제거

--- src/main/java/web/slack/config/WebsocketConfig.java
+++ src/main/java/web/slack/config/WebsocketConfig.java
@@ -1,7 +1,6 @@
 package web.slack.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
