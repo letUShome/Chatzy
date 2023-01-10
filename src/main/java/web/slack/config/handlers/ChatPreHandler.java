35f152112e67303b9aa4e1f402abc1294686347e
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2023-01-10 18:36:50 +0900

    Refactor: 토큰을 받고 체크하는 코드 깔끔하게 리팩토링
    related to #22

--- src/main/java/web/slack/config/handlers/ChatPreHandler.java
+++ src/main/java/web/slack/config/handlers/ChatPreHandler.java
@@ -1,11 +1,15 @@
 package web.slack.config.handlers;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.converter.CompositeMessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.core.Authentication;
@@ -16,10 +20,12 @@ import web.slack.domain.entity.Member;
 import web.slack.domain.entity.Profile;
 import web.slack.domain.repository.ProfileRepository;
 
+import javax.management.Notification;
 import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class ChatPreHandler implements ChannelInterceptor {
 
     private final ProfileRepository profileRepository;
@@ -32,21 +38,14 @@ public class ChatPreHandler implements ChannelInterceptor {
 
         // 헤더 토큰 얻기
         String authorHeader = String.valueOf(headerAccessor.getNativeHeader("Authorization"));
-        // 토큰 자르기 fixme 토큰 자르는 로직 validate 로 리팩토링
+        //헤더에서 토큰 얻었을 시, 대괄호가 쳐져있어서 이를 잘라내는 코드
         String token = authorHeader.replaceAll("\\[(.*?)\\]", "$1");
 
         if(jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token);
-            Member member = (Member)auth.getPrincipal();
-            Profile profile = profileRepository.findProfileByMemberId(member.getId()).orElseThrow(() -> new IllegalArgumentException("워크스페이스에 속하지 않은 유저입니다."));
-            MessageRequestDTO messageDTO = (MessageRequestDTO) message.getPayload();
-
-            if(Objects.equals(profile.getId(), messageDTO.getSender())) return message;
-            else throw new MessageDeliveryException("잘못된 유저로부터 온 메세지입니다");
-        }
-        else {
             return message;
         }
-
+        else {
+            throw new JwtException("유효한 토큰이 아닙니다");
+        }
     }
 }
